5038d998406a4376ddb9514b470d1712aa7b62b7
Nikhil0407 <nikhilkapre2002@gmail.com>
2024-03-11 17:55:01 +0530

    first commit

---
+++ admin/inc/footer.php
@@ -0,0 +1,18 @@
+
+
+    <div class="row bg-black text-center text-white">
+                <div class="col-12 my-3">
+                    <p> &copy; Copyright 2023 - All rights reserved <br />
+                </p>
+                </div>
+    </div>
+
+</div>
+
+
+<script src="..\assets\js\bootstrap.min.js"></script>
+<script src="..\assets\js\jquery.min.js"></script>
+
+
+</body>
+</html>
---
+++ voters/inc/footer.php
@@ -0,0 +1,14 @@
+
+    <div class="row bg-black text-center text-white">
+        <div class="col-12 my-3">
+            <p> &copy; Copyright 2022 - All Rights Reserved  <br />
+            </p>
+        </div>
+    </div>
+
+</div>
+
+<script src="../assets/js/jquery.min.js"></script>
+<script src="../assets/js/bootstrap.min.js"></script>
+</body>
+</html>
